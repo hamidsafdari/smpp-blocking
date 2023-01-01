73a155e52c347bf3c1afbb02d0ead7bee3c31237
Hamid Safdari <hamidsafdari22@gmail.com>
2023-01-01 17:21:22 +0430

    fix: cannot open project in fresh workspace

---
+++ src/test/kotlin/hs/jsmpp/ReactorTests.kt
@@ -0,0 +1,10 @@
+package hs.jsmpp
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+import org.slf4j.LoggerFactory
+
+@ExtendWith(MockitoExtension::class)
+internal class ReactorTests {
+    private val logger = LoggerFactory.getLogger(ReactorTests::class.java)
+}
